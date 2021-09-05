40e98347fbd27fb2f8a9014c73c9923671c35d27
Lara <lara@maditinternational.com>
2021-09-05 20:54:18 +0400

    started markap of contact persons

--- src/components/Counterparty/TabsForCreating/GeneralInformation.tsx
+++ src/components/Counterparty/TabsForCreating/GeneralInformation.tsx
@@ -1,34 +1,34 @@
 import React from "react";
-import { makeStyles,createStyles ,Theme} from "@material-ui/core/styles";
-import {Paper} from "@material-ui/core";
-import {WithMaterialUI} from "../Forms/GeneralInformation/FormGeneralInformation";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { Paper } from "@material-ui/core";
+import { WithMaterialUI } from "../Forms/GeneralInformation/FormGeneralInformation";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        padding:'1%',
-        '& > *': {
-           margin: theme.spacing(1),
-        },
+  createStyles({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      padding: "1%",
+      "& > *": {
+        margin: theme.spacing(1),
       },
-    }),
+    },
+  })
 );
 
 export const GeneralInformation = () => {
   const classes = useStyles();
-  return <div className={classes.root}>
-    <Paper style={{width:'23%',padding:10}}>
-        <WithMaterialUI/>
-    </Paper>
-      <Paper style={{width:'38%',padding:10}}>
-          <WithMaterialUI/>
+  return (
+    <div className={classes.root}>
+      <Paper style={{ width: "23%", padding: 10 }}>
+        <WithMaterialUI />
       </Paper>
-      <Paper style={{width:'33%',padding:10}}>
-          <WithMaterialUI/>
+      <Paper style={{ width: "38%", padding: 10 }}>
+        <WithMaterialUI />
       </Paper>
-
-
-  </div>;
+      <Paper style={{ width: "33%", padding: 10 }}>
+        <WithMaterialUI />
+      </Paper>
+    </div>
+  );
 };

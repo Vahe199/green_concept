e5f89ddd1c357f652e61f6a680355a68dd47f880
Vahagn <you@example.com>
2021-10-26 03:25:55 -0700

    validacion

--- src/components/Counterparty/TabsForCreating/TabsForUtil/ContactPersonsForCreatingValidate.tsx
+++ src/components/Counterparty/TabsForCreating/TabsForUtil/ContactPersonsForCreatingValidate.tsx
@@ -2,71 +2,64 @@ import * as yup from "yup";
 
 export const validationSchemaContactPerson = (value: any) => yup.object({
 
-  firstname: yup.string().required("Обязательное поле").max(100, "должен состоять максимум из 100 символов"),
-  surname: yup.string().required("Обязательное поле").max(100, "должен состоять максимум из 100 символов"),
-  birthdate: yup.string().required("Обязательное поле"),
+  firstname: yup.string().max(100, "должен состоять максимум из 100 символов"),
+  surname: yup.string().max(100, "должен состоять максимум из 100 символов"),
+  birthdate: yup.string(),
     branches: yup.array()
-        .of(yup.number().required("Обязательное поле"))
-        .strict()
-        .required("Обязательное поле"),
+        .of(yup.number())
+        .strict(),
   contact_congratulations: yup
     .array()
     .of(
       yup.object().shape({
-        name: yup.string().required("Обязательное поле"),
-        congratulation_type_id: yup.number().nullable(true).required("Обязательное поле"),
-        other: yup.string().required("Обязательное поле"),
+        name: yup.string(),
+        congratulation_type_id: yup.number().nullable(true),
+        other: yup.string(),
       })
-    )
-    .required("Обязательное поле"),
+    ),
+   
 
   contact_employees: yup
     .array()
     .of(
       yup.object().shape({
-        direction_id: yup.string().required("Обязательное поле"),
-        employee_id: yup.number().nullable(true).required("Обязательное поле"),
-        info: yup.string().required("Обязательное поле"),
+        direction_id: yup.string(),
+        employee_id: yup.number().nullable(true),
+        info: yup.string(),
       })
-    )
-    .required("Обязательное поле"),
+    ),
     contact_contractors:
       yup.object().shape({
           role_id: yup.number()
-              .nullable(true)
-              .required("Обязательное поле"),
-          position: yup.string().required("Обязательное поле").max(100, "должен состоять максимум из 100 символов"),
-          main: yup.number().nullable(true).required("Обязательное поле"),
+              .nullable(true),
+          position: yup.string().max(100, "должен состоять максимум из 100 символов"),
+          main: yup.number().nullable(true),
       }),
 
-  contractor_type_id: yup.string().required("Обязательное поле"),
-  delivery_address: yup.string().required("Обязательное поле"),
+  contractor_type_id: yup.string().required("Обязательно к заполнению"),
+  delivery_address: yup.string(),
   emails: yup
     .array()
     .of(
       yup.object().shape({
         email: yup
           .string()
-          .email("Неверный адрес электронной почты")
-          .required("Обязательное поле"),
+          .email("Неверный адрес электронной почты"),
         // salary: yup.string().min(3, 'cmon').required('Required'),
       })
-    )
-    .required("Обязательное поле"),
-  middlename: yup.string().required("Обязательное поле").max(100, "должен состоять максимум из 100 символов"),
+    ),
+  middlename: yup.string().max(100, "должен состоять максимум из 100 символов"),
   phones: yup
     .array()
     .of(
       yup.object().shape({
         phone: yup
           .string()
-          .min(10, "Слишком короткый")
-          .required("Обязательное поле"),
+          .min(10, "Слишком короткый"),
         // salary: yup.string().min(3, 'cmon').required('Required'),
       })
-    )
-    .required("Обязательное поле"),
-  service_type_id: value !== 1 ? yup.number().nullable(true).required("Обязательное поле") : yup.number().nullable(true),
-  sex: yup.string().required("Обязательное поле"),
-  status_id: yup.number().nullable(true).required("Обязательное поле"),
+    ),
+  service_type_id: value !== 1 ? yup.number().nullable(true) : yup.number().nullable(true),
+  sex: yup.string(),
+  status_id: yup.number().nullable(true),
 });
